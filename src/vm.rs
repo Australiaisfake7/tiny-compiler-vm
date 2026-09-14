98fed17636ec10583ca409ba1db18b6ddad3f252
Australiaisfake7 <michaeloverend900@gmail.com>
2026-09-14 14:47:55 +1200

    Added jumps to vm

--- src/vm.rs
+++ src/vm.rs
@@ -13,19 +13,23 @@ enum Value {
 
 #[derive(Debug)]
 enum VmError {
-    StackUnderflow, 
     StackOverflow,
+    NoValueOnStack,
     TypeMismatchUnary { opcode: String, operand: Value },
     TypeMismatchBinary { opcode: String, left: Value, right: Value },
+    TypeMisMatchJump(Value),
+    JumpOutOfBounds(usize),
 }
 
 impl fmt::Display for VmError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
-            VmError::StackUnderflow => write!(f, "stack underflow"),
             VmError::StackOverflow => write!(f, "stack overflow"),
+            VmError::NoValueOnStack => write!(f, "no value on stack"),
             VmError::TypeMismatchUnary { opcode, operand } => write!(f, "type mismatch of value {operand:?} with opcode {opcode}"),
             VmError::TypeMismatchBinary { opcode, left, right } => write!(f, "type mismatch of values {left:?} and {right:?} with opcode {opcode}"),
+            VmError::TypeMisMatchJump(operand) => write!(f, "type mismatch for conditional jump {operand:?}"),
+            VmError::JumpOutOfBounds(index) => write!(f, "jump index {index} out of bounds"),
         }
     }
 }
@@ -44,31 +48,39 @@ impl Stack {
     
     fn pop(&mut self) -> Result<Value, VmError> {
         if self.address == 0 {
-            return Err(VmError::StackUnderflow);
+            return Err(VmError::NoValueOnStack);
         }
 
         self.address -= 1;
         Ok(std::mem::take(self.data.get_mut(self.address).unwrap()))
     }
-    
     fn push(&mut self, value: Value) -> Result<(), VmError> {
-        if self.address >= STACK_SIZE {
-            return Err(VmError::StackOverflow);
-        }
-
-        *self.data.get_mut(self.address).unwrap() = value;
+        *self.data.get_mut(self.address).ok_or(VmError::StackOverflow)? = value;
+        self.address += 1;
         Ok(())
     }
-}
+    fn peek(&self) -> Result<&Value, VmError> {
+        if self.address == 0 {
+            return Err(VmError::NoValueOnStack)
+        }
 
-pub fn excecute(data: CompiledData) {
-    let mut stack: Stack = Stack::new();
-    for opcode in data.opcodes {
-        let _ = excecute_opcode(&opcode, &mut stack);
+        Ok(self.data.get(self.address - 1).unwrap())
     }
 }
 
-fn excecute_opcode(opcode: &OpCode, stack: &mut Stack) -> Result<(), VmError> {
+pub fn excecute(data: CompiledData) -> Result<(), VmError> {
+    let mut stack: Stack = Stack::new();
+    let mut ptr: usize = 0;
+    loop {
+        let opcode: &OpCode = data.opcodes.get(ptr).ok_or(VmError::JumpOutOfBounds(ptr))?;
+        ptr += 1;
+        excecute_opcode(opcode, &mut stack, &mut ptr)?;
+    }
+
+    Ok(())
+}
+
+fn excecute_opcode(opcode: &OpCode, stack: &mut Stack, ptr: &mut usize) -> Result<(), VmError> {
     match opcode {
         OpCode::LNot | OpCode::Negate => {
             let a = stack.pop()?;
@@ -92,7 +104,10 @@ fn excecute_opcode(opcode: &OpCode, stack: &mut Stack) -> Result<(), VmError> {
                     right 
                 })?;
             stack.push(result)?;
-        }
+        },
+        OpCode::Jump(index) => { *ptr = *index; },
+        OpCode::JumpIfFalse { index, pop } => jump_conditonal(stack, *index, ptr, &false, *pop)?,
+        OpCode::JumpIfTrue { index, pop } => jump_conditonal(stack, *index, ptr, &true, *pop)?,
     };
 
     Ok(())
@@ -143,4 +158,18 @@ fn apply_binary(opcode: &OpCode, a: Value, b: Value) -> Result<Value, (Value, Va
 
         (_, a, b) => Err((a, b)),
     }
+}
+
+fn jump_conditonal(stack: &mut Stack, index: usize, ptr: &mut usize, jump_on: &bool, pop: bool) -> Result<(), VmError> {
+    let condition: &Value = if pop { &stack.pop()? } else { stack.peek()? };
+
+    if let Value::Bool(b) = condition {
+        if b == jump_on {
+            *ptr = index;   
+        }
+
+        return Ok(());
+    }
+
+    Err(VmError::TypeMisMatchJump(condition.clone()))
 }
